65ada699ecacb77dfd6f86e3a5dade0340cb316a
Omkar A. Katta <omkarakatta@gmail.com>
2021-02-20 11:36:05 -0500

    add todo: check for local config file, else look for package file
    - move format-dependent files to package storage in inst/...

--- R/render_fcns.R
+++ R/render_fcns.R
@@ -13,6 +13,8 @@
 
 ### render_note ---------------------------
 
+#~ TODO: Check for the existence of a local _bookdown.yml or _output.yml file;
+#~    if there is no such file, then look for package defaults
 render_note <- function(input = ".",
                         config_file = "note-bookdown.yml",
                         output_yaml = "note-output.yml",
