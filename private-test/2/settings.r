79cc28dc8c59a0caecf4686fe2f470e68b02f94a
Omkar A. Katta <omkarakatta@gmail.com>
2021-04-03 15:13:13 -0400

    Move index configuration to skeleton [note]

---
+++ private-test/2/settings.r
@@ -0,0 +1,20 @@
+### {knitr} Options ---------------------------
+
+knitr::opts_chunk$set(cache = FALSE,
+                      collapse = TRUE,
+                      tidy = 'styler')
+
+### {tinytex} Options ---------------------------
+
+options(tinytex.clean = FALSE) # save intermediate files
+
+### Chunk Font Size ---------------------------
+
+knitr::knit_hooks$set(mysize = function(before, options, envir) {
+  if (before) {
+    return(options$size)
+  } else {
+    return("\\normalsize")
+  }
+})
+knitr::opts_chunk$set(mysize = TRUE, size = "\\footnotesize")
