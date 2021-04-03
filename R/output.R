68e7a03ec2851aec384516c2f1806c2a49f10b2f
Omkar A. Katta <omkarakatta@gmail.com>
2021-04-03 16:36:56 -0400

    Create pdf_note to wrap pdf_output [note]

--- R/output.R
+++ R/output.R
@@ -11,27 +11,42 @@
 ### pdf_output -------------------------
 #' Convert RMD to PDF Output with {oakdown} templates
 #'
-#' @param template Name of {oakdown} template
+#' @param format Name of {oakdown} template
 #' @param keep_tex,keep_md,dev,citation_package,pandoc_args,md_extensions,...
 #'  Arguments to be passed onto bookdown::pdf_book
 #'
 #' @export
-pdf_output <- function(template = "note",
+pdf_output <- function(format = "note",
                        keep_tex = TRUE,
                        keep_md = TRUE,
                        dev = "cairo_pdf",
                        citation_package = "natbib",
-                       pandoc_args = c("--listings", "--shift-heading-level-by=-1"),
+                       pandoc_args = c("--listings"),
                        md_extensions = "-auto_identifiers",
                        ...) {
   bookdown::pdf_book(
-    template = resource_path(template),
+    template = resource_path(format),
     keep_tex = keep_tex,
     keep_md = keep_md,
     dev = dev,
     citation_package = citation_package,
     pandoc_args = pandoc_args,
     md_extensions = md_extensions,
+    ...
+  )
+}
+
+### pdf_note -------------------------
+#' Convert Rmd into a note
+#'
+#' @param ... Arguments to be passed to \code{\link{pdf_output}}
+#'
+#' @export
+pdf_note <- function(...) {
+  pdf_output(
+    format = "note",
+    pandoc_args = c("--listings",
+                    "--shift-heading-level-by=-1"),
     ...
   )
 }
