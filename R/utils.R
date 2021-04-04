d5d12dc48058f52c86ec9c4c2941c734991b011d
Omkar A. Katta <omkarakatta@gmail.com>
2021-04-04 09:15:48 -0400

    Generalize template_resources

--- R/utils.R
+++ R/utils.R
@@ -11,17 +11,20 @@
 #' Find resources within {oakdown} template
 #'
 #' @param template Name of template
-#' @param file Name of file in "resources" directory of \code{template}
+#' @param file Name of file in \code{subdir} directory of \code{template}
+#' @param subdir Name of subdirectory in \code{template}
 #'
 #' @return File path of \code{file} located in {oakdown}'s
 #'  \code{template} directory
 #'
 #' @export
-template_resources <- function(template, file = "pandoc.tex") {
+template_resources <- function(template,
+                               file = "pandoc.tex",
+                               subdir = "resources") {
   fs::path_package(package = "oakdown",
                    "rmarkdown",
                    "templates",
                    template,
-                   "resources",
+                   subdir,
                    file)
 }
