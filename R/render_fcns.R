3498655bd93f7acf89618523f8b253d8fc876749
Omkar A. Katta <omkarakatta@gmail.com>
2021-04-04 08:53:35 -0400

    Update documentation [render_note]

--- R/render_fcns.R
+++ R/render_fcns.R
@@ -15,6 +15,20 @@
 
 #~ TODO: Check for the existence of a local _bookdown.yml or _output.yml file;
 #~    if there is no such file, then look for package defaults
+#' Render Rmd to a PDF note
+#'
+#' @param input Input filename or directory, to be passed to
+#'  \code{bookdown::render_book()}
+#' @param config_file Book configuration file
+#' @param clean Argument to be passed to \code{tinytex.clean} option;
+#'  if FALSE (default), keep intermediary files
+#' @param title_fcn Function to interpret header information in Rmd
+#' @param ... Arguments to be passed to \code{bookdown::render_book}
+#'
+#' @return A PDF note with the name and directory as specified by
+#'  \code{config_file}
+#'
+#' @export
 render_note <- function(input = ".",
                         config_file = "note-bookdown.yml",
                         clean = FALSE,
